f13b7227432c7286e64ed2dbc18b4f66f223475d
Finn Kristian Meland <>
2018-09-05 18:00:08 +0200

    Initial commit

---
+++ syslog.go
@@ -0,0 +1,86 @@
+// +build !windows,!nacl,!plan9
+
+package syslog
+
+import (
+	"log"
+	"log/syslog"
+
+	"github.com/sirupsen/logrus"
+)
+
+// We are logging to syslog, strip colors to make the output more readable.
+var defaultFormatter = &logrus.TextFormatter{DisableColors: true}
+
+// SyslogHook to send logs via syslog.
+type SyslogHook struct {
+	Writer        *syslog.Writer
+	SyslogNetwork string
+	SyslogRaddr   string
+	formatter     logrus.Formatter
+}
+
+// Creates a hook to be added to an instance of logger. This is called with
+// `hook, err := NewSyslogHook("udp", "localhost:514", syslog.LOG_DEBUG, "")`
+// `if err == nil { log.Hooks.Add(hook) }`
+func NewSyslogHook(network, raddr string, priority syslog.Priority, tag string, formatter logrus.Formatter) (*SyslogHook, error) {
+	w, err := syslog.Dial(network, raddr, priority, tag)
+	hook := &SyslogHook{
+		Writer:        w,
+		SyslogNetwork: network,
+		SyslogRaddr:   raddr,
+	}
+	hook.SetFormatter(formatter)
+	return hook, err
+}
+
+// SetFormatter sets the format that will be used by hook.
+// If using text formatter, this method will disable color output to make the log file more readable.
+func (hook *SyslogHook) SetFormatter(formatter logrus.Formatter) {
+	if formatter == nil {
+		formatter = defaultFormatter
+	} else {
+		switch formatter.(type) {
+		case *logrus.TextFormatter:
+			textFormatter := formatter.(*logrus.TextFormatter)
+			textFormatter.DisableColors = true
+		}
+	}
+
+	hook.formatter = formatter
+}
+
+func (hook *SyslogHook) Fire(entry *logrus.Entry) error {
+
+	hook.formatter.Format(entry)
+
+	// use our formatter instead of entry.String()
+	msg, err := hook.formatter.Format(entry)
+	if err != nil {
+		log.Println("failed to generate string for entry:", err)
+		return err
+	}
+
+	line := string(msg[:])
+
+	switch entry.Level {
+	case logrus.PanicLevel:
+		return hook.Writer.Crit(line)
+	case logrus.FatalLevel:
+		return hook.Writer.Crit(line)
+	case logrus.ErrorLevel:
+		return hook.Writer.Err(line)
+	case logrus.WarnLevel:
+		return hook.Writer.Warning(line)
+	case logrus.InfoLevel:
+		return hook.Writer.Info(line)
+	case logrus.DebugLevel:
+		return hook.Writer.Debug(line)
+	default:
+		return nil
+	}
+}
+
+func (hook *SyslogHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
